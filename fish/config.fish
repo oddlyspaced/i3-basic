0aaeb58759dcb989ba7b4505e99162be7e5cbdcb
oddlyspaced <oddlyspaced1@gmail.com>
2020-09-23 01:15:35 +0530

    fish: Add updated fish config with variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 ufetch | lolcat
 set -x EDITOR nvim
 set -x PATH /home/hardik/.gem/ruby/2.7.0/bin:$PATH
+set -x PATH /home/hardik/Softwares/flutter/bin:$PATH
+set -x PATH /home/hardik/Softwares/flutter/bin/cache/dart-sdk/bin:$PATH
+set -x PATH /home/hardik/.pub-cache/bin:$PATH
+set -x CHROME_EXECUTABLE google-chrome-stable
