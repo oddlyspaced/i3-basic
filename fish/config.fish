225ac11e245d7f9a0531bae8c692e918567357f6
oddlyspaced <oddlyspaced1@gmail.com>
2021-11-18 00:04:12 +0530

    Revert "fish: Alias ls for ls -l"
    This reverts commit 716e72f7515ca3c979604a9383d5278803924932.

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,7 @@
 ufetch | lolcat
-set -U fish_greeting ""
 set -x EDITOR nvim
 set -x PATH /home/hardik/.gem/ruby/2.7.0/bin:$PATH
 set -x PATH /home/hardik/Softwares/flutter/bin:$PATH
 set -x PATH /home/hardik/Softwares/flutter/bin/cache/dart-sdk/bin:$PATH
 set -x PATH /home/hardik/.pub-cache/bin:$PATH
-set -x ANDROID_SDK_ROOT /home/hardik/Softwares/Android
 set -x CHROME_EXECUTABLE google-chrome-stable
-alias ls='ls -l'
