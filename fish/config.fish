30dbde1c0c532344b7fbffcd2de29fcfa0fe9a30
oddlyspaced <oddlyspaced1@gmail.com>
2021-11-18 00:06:10 +0530

    fish: Set Android Studio path variable

--- fish/config.fish
+++ fish/config.fish
@@ -5,4 +5,5 @@ set -x PATH /home/hardik/.gem/ruby/2.7.0/bin:$PATH
 set -x PATH /home/hardik/Softwares/flutter/bin:$PATH
 set -x PATH /home/hardik/Softwares/flutter/bin/cache/dart-sdk/bin:$PATH
 set -x PATH /home/hardik/.pub-cache/bin:$PATH
+set -x ANDROID_SDK_ROOT /home/hardik/Android/Sdk
 set -x CHROME_EXECUTABLE google-chrome-stable
